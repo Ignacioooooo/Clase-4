375161a0cb4183f4d860db07a7594c6f87ac2a51
Ignacioooooo <107085341+Ignacioooooo@users.noreply.github.com>
2023-03-23 23:20:18 -0300

    Se avanzó

--- src/Carrito.java
+++ src/Carrito.java
@@ -1,3 +1,14 @@
 public class Carrito {
+ public static void main(String[] args) {
+  Productos miObjeto = new Productos();
+  String[] datos = {"Jabón en polvo", "Esponjas", "Chocolates"};
+  miObjeto.llenarArray(datos);
 
+System.out.println("Tiene en su carrito:");
+for (String Producto : datos){
+ System.out.println(Producto);
 }
+ }
+}
+
+
--- src/Productos.java
+++ src/Productos.java
@@ -1,3 +1,14 @@
-public class Productos {
 
+
+public class Productos{
+    private String[] miArray;
+
+    public void llenarArray(String[] datos) {
+        miArray = datos;
+    }
+
+    public String[] obtenerArray() {
+        return miArray;
+    }
 }
+
